082e1b783d54f155d7f3bd9524434ef4a1943b2c
A00833574 <A00833574@tec.mx>
2023-08-20 16:39:06 -0600

    Added Chargers Feature and Communication

--- bot_cleaners/model.py
+++ bot_cleaners/model.py
@@ -3,8 +3,15 @@ from mesa.agent import Agent
 from mesa.space import MultiGrid
 from mesa.time import SimultaneousActivation
 from mesa.datacollection import DataCollector
+from math import sqrt
 
 import numpy as np
+
+
+class Cargador(Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+
 
 
 class Celda(Agent):
@@ -24,6 +31,7 @@ class RobotLimpieza(Agent):
         self.sig_pos = None
         self.movimientos = 0
         self.carga = 100
+        self.cargando = False
 
     def limpiar_una_celda(self, lista_de_celdas_sucias):
         celda_a_limpiar = self.random.choice(lista_de_celdas_sucias)
@@ -32,6 +40,30 @@ class RobotLimpieza(Agent):
 
     def seleccionar_nueva_pos(self, lista_de_vecinos):
         self.sig_pos = self.random.choice(lista_de_vecinos).pos
+
+    def buscar_cargador(self, cargadores):
+        distancias = []
+        for i in cargadores:
+            # Calula la distancia entre el robot y los cargadores
+            xi, yi = self.pos
+            xf, yf = i.pos
+            dist = sqrt((xf - xi) ** 2 + (yf - yi) ** 2)
+            distancias.append(dist)
+        # Obtiene el cargador mas cercano
+        cargador_cercano = cargadores[distancias.index(min(distancias))]
+        return cargador_cercano
+
+    def moverse_a(self, pos):
+        xf, yf = pos
+        xi, yi = self.pos
+        if xf > xi:
+            self.sig_pos = (xi + 1, yi)
+        elif xf < xi:
+            self.sig_pos = (xi - 1, yi)
+        elif yf > yi:
+            self.sig_pos = (xi, yi + 1)
+        elif yf < yi:
+            self.sig_pos = (xi, yi - 1)
 
     @staticmethod
     def buscar_celdas_sucia(lista_de_vecinos):
@@ -46,45 +78,115 @@ class RobotLimpieza(Agent):
         return celdas_sucias
 
     def step(self):
-        vecinos = self.model.grid.get_neighbors(
-            self.pos, moore=True, include_center=False)
 
-        for vecino in vecinos:
-            if isinstance(vecino, (Mueble, RobotLimpieza)):
-                vecinos.remove(vecino)
-
-        celdas_sucias = self.buscar_celdas_sucia(vecinos)
-
-        if len(celdas_sucias) == 0:
-            self.seleccionar_nueva_pos(vecinos)
+        if self.cargando:
+            if self.carga < 100:
+                self.carga += 1
+            else:
+                self.cargando = False
         else:
-            self.limpiar_una_celda(celdas_sucias)
+            vecinos = self.model.grid.get_neighbors(
+                self.pos, moore=True, include_center=False
+            )
+
+            for vecino in vecinos:
+                if isinstance(vecino, (Mueble, RobotLimpieza)):
+                    vecinos.remove(vecino)
+
+            celdas_sucias = self.buscar_celdas_sucia(vecinos)
+
+            if len(celdas_sucias) == 0:
+                compañeros = [] # Lista de compañeros
+                for content, pos in self.model.grid.coord_iter():
+                    for obj in content:
+                        if isinstance(obj, RobotLimpieza) and obj.unique_id != self.unique_id:
+                            compañeros.append(obj)
+
+                for robot in compañeros:
+                    vecinos = self.model.grid.get_neighbors(
+                        robot.pos, moore=True, include_center=False
+                    )
+                    for vecino in vecinos:
+                        if isinstance(vecino, (Mueble, RobotLimpieza)):
+                            vecinos.remove(vecino)
+                    celdas_sucias = robot.buscar_celdas_sucia(vecinos)
+                    if len(celdas_sucias) > 0:
+                        self.moverse_a(robot.pos)
+                        break
+            else:
+                self.limpiar_una_celda(celdas_sucias)
+
+        if self.carga < 25:
+            cargadores = [ ]
+            for content, pos in self.model.grid.coord_iter():
+                for obj in content:
+                    if isinstance(obj, Cargador):
+                        cargadores.append(obj)
+            cargador = self.buscar_cargador(cargadores)
+            self.moverse_a(cargador.pos)
+            if self.pos == cargador.pos:
+                self.cargando = True
+
 
     def advance(self):
         if self.pos != self.sig_pos:
             self.movimientos += 1
 
-        if self.carga > 0:
-            self.carga -= 1
-            self.model.grid.move_agent(self, self.sig_pos)
+        if not self.cargando:
+            if self.carga > 0:
+                self.carga -= 1
+                self.model.grid.move_agent(self, self.sig_pos)
 
 
 class Habitacion(Model):
-    def __init__(self, M: int, N: int,
-                 num_agentes: int = 5,
-                 porc_celdas_sucias: float = 0.6,
-                 porc_muebles: float = 0.1,
-                 modo_pos_inicial: str = 'Fija',
-                 ):
-
+    def __init__(
+        self,
+        M: int,
+        N: int,
+        num_agentes: int = 5,
+        porc_celdas_sucias: float = 0.6,
+        porc_muebles: float = 0.1,
+        modo_pos_inicial: str = "Fija",
+        num_cargadores: int = 4,
+    ):
         self.num_agentes = num_agentes
         self.porc_celdas_sucias = porc_celdas_sucias
         self.porc_muebles = porc_muebles
+        self.num_cargadores = num_cargadores
 
         self.grid = MultiGrid(M, N, False)
         self.schedule = SimultaneousActivation(self)
 
         posiciones_disponibles = [pos for _, pos in self.grid.coord_iter()]
+
+        # Posicionamiento de cargadores
+
+        # Dividir el plano en cuatro cuadrantes
+        cuadrante_1 = []
+        cuadrante_2 = []
+        cuadrante_3 = []
+        cuadrante_4 = []
+        for i in range(M):
+            for j in range(N):
+                if i < M / 2 and j < N / 2:
+                    cuadrante_1.append((i, j))
+                elif i < M / 2 and j >= N / 2:
+                    cuadrante_2.append((i, j))
+                elif i >= M / 2 and j < N / 2:
+                    cuadrante_3.append((i, j))
+                elif i >= M / 2 and j >= N / 2:
+                    cuadrante_4.append((i, j))
+        # Colocar un cargador en cada cuadrante
+        posiciones_cargadores = []
+        posiciones_cargadores.append(self.random.choice(cuadrante_1))
+        posiciones_cargadores.append(self.random.choice(cuadrante_2))
+        posiciones_cargadores.append(self.random.choice(cuadrante_3))
+        posiciones_cargadores.append(self.random.choice(cuadrante_4))
+        # Colocar los cargadores
+        for id, pos in enumerate(posiciones_cargadores):
+            cargador = Cargador(int(f"{num_agentes}00{id}") + 1, self)
+            self.grid.place_agent(cargador, pos)
+            posiciones_disponibles.remove(pos)
 
         # Posicionamiento de muebles
         num_muebles = int(M * N * porc_muebles)
@@ -98,16 +200,25 @@ class Habitacion(Model):
         # Posicionamiento de celdas sucias
         self.num_celdas_sucias = int(M * N * porc_celdas_sucias)
         posiciones_celdas_sucias = self.random.sample(
-            posiciones_disponibles, k=self.num_celdas_sucias)
+            posiciones_disponibles, k=self.num_celdas_sucias
+        )
 
         for id, pos in enumerate(posiciones_disponibles):
             suciedad = pos in posiciones_celdas_sucias
             celda = Celda(int(f"{num_agentes}{id}") + 1, self, suciedad)
             self.grid.place_agent(celda, pos)
 
+        """
+        Contar celdas sucias
+        Reducir el contador con cada celda que se limpie
+        Detener tiempo cuando el contador llegue a 0
+        """
+
         # Posicionamiento de agentes robot
-        if modo_pos_inicial == 'Aleatoria':
-            pos_inicial_robots = self.random.sample(posiciones_disponibles, k=num_agentes)
+        if modo_pos_inicial == "Aleatoria":
+            pos_inicial_robots = self.random.sample(
+                posiciones_disponibles, k=num_agentes
+            )
         else:  # 'Fija'
             pos_inicial_robots = [(1, 1)] * num_agentes
 
@@ -117,17 +228,24 @@ class Habitacion(Model):
             self.schedule.add(robot)
 
         self.datacollector = DataCollector(
-            model_reporters={"Grid": get_grid, "Cargas": get_cargas,
-                             "CeldasSucias": get_sucias},
+            model_reporters={
+                "Grid": get_grid,
+                "Cargas": get_cargas,
+                "CeldasSucias": get_sucias,
+            },
         )
 
     def step(self):
         self.datacollector.collect(self)
 
-        self.schedule.step()
+        for agent in self.schedule.agents:
+            agent.step()
+
+        for agent in self.schedule.agents:
+            agent.advance()
 
     def todoLimpio(self):
-        for (content, x, y) in self.grid.coord_iter():
+        for content, x, y in self.grid.coord_iter():
             for obj in content:
                 if isinstance(obj, Celda) and obj.sucia:
                     return False
--- bot_cleaners/server.py
+++ bot_cleaners/server.py
@@ -1,19 +1,39 @@
 import mesa
 
-from .model import Habitacion, RobotLimpieza, Celda, Mueble
+from .model import Habitacion, RobotLimpieza, Celda, Mueble, Cargador
 
 MAX_NUMBER_ROBOTS = 20
 
 
 def agent_portrayal(agent):
     if isinstance(agent, RobotLimpieza):
-        return {"Shape": "circle", "Filled": "false", "Color": "Cyan", "Layer": 1, "r": 0.9,
-                "text": f"{agent.carga}", "text_color": "black"}
+        return {
+            "Shape": "circle",
+            "Filled": "false",
+            "Color": "Cyan",
+            "Layer": 1,
+            "r": 0.9,
+            "text": f"{agent.carga}",
+            "text_color": "black",
+        }
     elif isinstance(agent, Mueble):
-        return {"Shape": "rect", "Filled": "true", "Color": "black", "Layer": 0,
-                "w": 0.9, "h": 0.9}
+        return {
+            "Shape": "rect",
+            "Filled": "true",
+            "Color": "black",
+            "Layer": 0,
+            "w": 0.9,
+            "h": 0.9,
+        }
     elif isinstance(agent, Celda):
-        portrayal = {"Shape": "rect", "Filled": "true", "Layer": 0, "w": 0.9, "h": 0.9, "text_color": "Black"}
+        portrayal = {
+            "Shape": "rect",
+            "Filled": "true",
+            "Layer": 0,
+            "w": 0.9,
+            "h": 0.9,
+            "text_color": "Black",
+        }
         if agent.sucia:
             portrayal["Color"] = "#ccbeaf"
             portrayal["text"] = "💩"
@@ -21,36 +41,48 @@ def agent_portrayal(agent):
             portrayal["Color"] = "white"
             portrayal["text"] = ""
         return portrayal
+    elif isinstance(agent, Cargador):
+        portrayal = {
+            "Shape": "rect",
+            "Filled": "true",
+            "Layer": 0,
+            "w": 0.9,
+            "h": 0.9,
+            "text_color": "Black",
+            "Color" : "green",
+            "text" : "🔋",
+        }
+        return portrayal
 
 
-grid = mesa.visualization.CanvasGrid(
-    agent_portrayal, 20, 20, 400, 400)
+grid = mesa.visualization.CanvasGrid(agent_portrayal, 20, 20, 400, 400)
 chart_celdas = mesa.visualization.ChartModule(
-    [{"Label": "CeldasSucias", "Color": '#36A2EB', "label": "Celdas Sucias"}],
-    50, 200,
-    data_collector_name="datacollector"
+    [{"Label": "CeldasSucias", "Color": "#36A2EB", "label": "Celdas Sucias"}],
+    50,
+    200,
+    data_collector_name="datacollector",
 )
 
 model_params = {
     "num_agentes": mesa.visualization.Slider(
         "Número de Robots",
-        5,
-        2,
-        MAX_NUMBER_ROBOTS,
-        1,
+        5,  # Valor por defecto *5*
+        2,  # Valor mínimo
+        MAX_NUMBER_ROBOTS,  # Valor máximo
+        1,  # Incremento
         description="Escoge cuántos robots deseas implementar en el modelo",
     ),
     "porc_celdas_sucias": mesa.visualization.Slider(
         "Porcentaje de Celdas Sucias",
-        0.3,
-        0.0,
-        0.75,
-        0.05,
+        0.3, # Valor por defecto *0.3*
+        0.1, # Valor mínimo
+        0.75, # Valor máximo
+        0.05, # Incremento
         description="Selecciona el porcentaje de celdas sucias",
     ),
     "porc_muebles": mesa.visualization.Slider(
         "Porcentaje de Muebles",
-        0.1,
+        0.1, # Valor por defecto *0.1*
         0.0,
         0.25,
         0.01,
@@ -60,13 +92,12 @@ model_params = {
         "Posición Inicial de los Robots",
         "Aleatoria",
         ["Fija", "Aleatoria"],
-        "Seleciona la forma se posicionan los robots"
+        "Seleciona la forma se posicionan los robots",
     ),
     "M": 20,
     "N": 20,
 }
 
 server = mesa.visualization.ModularServer(
-    Habitacion, [grid, chart_celdas],
-    "botCleaner", model_params, 8521
+    Habitacion, [grid, chart_celdas], "botCleaner", model_params, 8521
 )
